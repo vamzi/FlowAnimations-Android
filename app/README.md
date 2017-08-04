b744784d4a617c01ec2dfeba16c1190f28b645a3
Vamsi Krishna Kothapalli <kothapallivamsikrishna@gmail.com>
2017-08-04 12:17:41 +0530

    Initial test push to GitHub!

---
+++ app/README.md
@@ -0,0 +1,2 @@
+# FlowAnimations
+A simple android library to animate views into the screen from outside parent views.
---
+++ app/src/main/java/vamzi/flowanimationsexample/MainActivity.java
@@ -0,0 +1,25 @@
+package vamzi.flowanimationsexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
+
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv=(TextView)findViewById(R.id.tv);
+
+
+    }
+
+
+}
